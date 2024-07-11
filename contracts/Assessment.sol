39789d038bd1aa0ae93ee91273cff8a8d87cd5b0
Jester Cruzate <trolarchefxthpoo@gmail.com>
2024-07-11 05:49:42 +0000

    Major Updates

--- contracts/Assessment.sol
+++ contracts/Assessment.sol
@@ -6,13 +6,27 @@ pragma solidity ^0.8.9;
 contract Assessment {
     address payable public owner;
     uint256 public balance;
+    bool public isLocked;
 
     event Deposit(uint256 amount);
     event Withdraw(uint256 amount);
+    event BalanceReset();
+    event ContractLocked();
+    event ContractUnlocked();
 
     constructor(uint initBalance) payable {
         owner = payable(msg.sender);
         balance = initBalance;
+        isLocked = false;
+    }
+    modifier onlyOwner() {
+        require(msg.sender == owner, "You are not the owner of this account");
+        _;
+    }
+
+    modifier whenNotLocked() {
+        require(!isLocked, "Contract is locked");
+        _;
     }
 
     function getBalance() public view returns(uint256){
@@ -57,4 +71,31 @@ contract Assessment {
         // emit the event
         emit Withdraw(_withdrawAmount);
     }
+    //resets the current balance to 0
+    function resetBalance() public {
+        require(msg.sender == owner, "You are not the owner of this account");
+        uint256 _currentBalance = balance;
+        if (_currentBalance == 0) {
+            revert InsufficientBalance({
+                balance: balance,
+                withdrawAmount: 0
+            });
+        }
+        balance = 0;
+        emit Withdraw(_currentBalance);
+        emit BalanceReset();
+    }
+
+    //locks the account and no transaction can through
+    function lock() public onlyOwner {
+        isLocked = true;
+        emit ContractLocked();
+    }
+
+    //opens the account 
+    function unlock() public onlyOwner {
+        isLocked = false;
+        emit ContractUnlocked();
+    }
+
 }
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ export default function HomePage() {
   const [account, setAccount] = useState(undefined);
   const [atm, setATM] = useState(undefined);
   const [balance, setBalance] = useState(undefined);
+  const [depositAmount, setDepositAmount] = useState("");
+  const [withdrawAmount, setWithdrawAmount] = useState("");
+  const [isLocked, setIsLocked] = useState(false);
 
   const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
   const atmABI = atm_abi.abi;
@@ -49,36 +52,88 @@ export default function HomePage() {
     const provider = new ethers.providers.Web3Provider(ethWallet);
     const signer = provider.getSigner();
     const atmContract = new ethers.Contract(contractAddress, atmABI, signer);
- 
     setATM(atmContract);
   }
 
-  const getBalance = async() => {
+  const getBalance = async () => {
     if (atm) {
-      setBalance((await atm.getBalance()).toNumber());
+      const balanceWei = await atm.getBalance();
+      const balanceEther = ethers.utils.formatEther(balanceWei);
+      const balanceFloat = parseFloat(balanceEther);
+      setBalance(balanceFloat.toFixed(4)); // Display up to 4 decimal places
     }
   }
-
-  const deposit = async() => {
-    if (atm) {
-      let tx = await atm.deposit(1);
-      await tx.wait()
-      getBalance();
+  const deposit = async () => {
+    if (atm && depositAmount !== "") {
+        let tx = await atm.deposit(ethers.utils.parseEther(depositAmount));
+        await tx.wait();
+        setDepositAmount(""); // Clear input field after deposit
+        getBalance();
     }
   }
 
   const withdraw = async() => {
-    if (atm) {
-      let tx = await atm.withdraw(1);
+    if (atm && withdrawAmount !== "") {
+      let tx = await atm.withdraw(ethers.utils.parseEther(withdrawAmount));
       await tx.wait()
+      setWithdrawAmount(""); // Clear input field after withdraw
       getBalance();
+    }
+  }
+
+  const getLockStatus = async () => {
+    if (atm) {
+      const lockStatus = await atm.isLocked();
+      setIsLocked(lockStatus);
+    }
+  }
+
+  const resetBalance = async () => {
+    if (atm) {
+      const confirmation = confirm("Are you sure you want to reset the balance?");
+      if (confirmation) {
+        try {
+          let tx = await atm.resetBalance();
+          await tx.wait();
+          getBalance();
+        } catch (error) {
+          if (error.code === ethers.errors.CALL_EXCEPTION) {
+            alert("You have insufficient balance.");
+          } else {
+            console.error(error);
+            alert("An error occurred while resetting the balance.");
+          }
+        }
+      }
+    }
+  }
+
+  const lockContract = async () => {
+    if (atm) {
+      const confirmation = confirm("Are you sure you want to lock the contract?");
+      if (confirmation) {
+        let tx = await atm.lock();
+        await tx.wait();
+        getLockStatus();
+      }
+    }
+  }
+
+  const unlockContract = async () => {
+    if (atm) {
+      const confirmation = confirm("Are you sure you want to unlock the contract?");
+      if (confirmation) {
+        let tx = await atm.unlock();
+        await tx.wait();
+        getLockStatus();
+      }
     }
   }
 
   const initUser = () => {
     // Check to see if user has Metamask
     if (!ethWallet) {
-      return <p>Please install Metamask in order to use this ATM.</p>
+      return <p>Please install Metamask in order to use this Savings Account.</p>
     }
 
     // Check to see if user is connected. If not, connect to their account
@@ -93,9 +148,88 @@ export default function HomePage() {
     return (
       <div>
         <p>Your Account: {account}</p>
-        <p>Your Balance: {balance}</p>
-        <button onClick={deposit}>Deposit 1 ETH</button>
-        <button onClick={withdraw}>Withdraw 1 ETH</button>
+        <p>Current Balance: {balance} ETH</p>
+        <p>Contract Status: {isLocked ? "Locked" : "Unlocked"}</p>
+        <input type="number" placeholder="Enter deposit amount" value={depositAmount} onChange={(e) => setDepositAmount(e.target.value)} 
+        style={{
+            margin: '8px',
+            padding: '6px 12px',
+            fontSize: '16px',
+            textAlign: "center",
+            border: '1px solid #ccc',
+            borderRadius: '20px',
+            boxSizing: 'border-box',
+            width: '300px', 
+          }}/>
+        <button onClick={deposit} disabled={isLocked}
+        style={{
+          margin: '8px',
+          padding: '6px 12px',
+          fontSize: '16px',
+          border: '1px solid #ccc',
+          borderRadius: '10px',
+          color: '#000',
+          cursor: 'pointer',
+        }}
+        >Deposit</button>
+        <br />
+        <input type="number" placeholder="Enter withdrawal amount" value={withdrawAmount} onChange={(e) => setWithdrawAmount(e.target.value)} 
+        style={{
+          margin: '8px',
+          padding: '6px 12px',
+          fontSize: '16px',
+          textAlign: "center",
+          border: '1px solid #ccc',
+          borderRadius: '20px',
+          boxSizing: 'border-box',
+          width: '300px', 
+        }}
+        />
+        <button onClick={withdraw} disabled={isLocked}
+        style={{
+          margin: '8px',
+          padding: '6px 12px',
+          fontSize: '16px',
+          border: '1px solid #ccc',
+          borderRadius: '10px',
+          color: '#000',
+          cursor: 'pointer',
+        }}
+        >Withdraw</button>
+        <br />
+        <button onClick={resetBalance} disabled={isLocked}
+        style={{
+          margin: '8px',
+          padding: '6px 12px',
+          fontSize: '16px',
+          border: '1px solid #ccc',
+          borderRadius: '10px',
+          color: '#000',
+          cursor: 'pointer',
+        }}
+        >Reset Balance</button>
+        <button onClick={lockContract}
+        style={{
+          margin: '8px',
+          padding: '6px 12px',
+          fontSize: '16px',
+          border: '1px solid #ccc',
+          borderRadius: '10px',
+          color: '#000',
+          cursor: 'pointer',
+        }}
+        >Lock Contract</button>
+        <button onClick={unlockContract}
+        style={{
+          margin: '8px',
+          padding: '6px 12px',
+          fontSize: '16px',
+          border: '1px solid #ccc',
+          borderRadius: '10px',
+          color: '#000',
+          cursor: 'pointer',
+        }}
+        >Unlock Contract</button>
       </div>
     )
   }
@@ -104,14 +238,26 @@ export default function HomePage() {
 
   return (
     <main className="container">
-      <header><h1>Welcome to the Metacrafters ATM!</h1></header>
+      <header><h1>ExMachina Savings Account</h1></header>
       {initUser()}
       <style jsx>{`
-        .container {
-          text-align: center
-        }
-      `}
-      </style>
+      .container {
+        text-align: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        padding-top: 20px;
+        min-height: 100vh;
+        text-align: center;
+        align-items: center;
+        font-family: 'Inter', sans-serif;
+      }
+
+      h1 {
+        font-size: 50px;
+      }
+      
+    `}</style>
     </main>
   )
 }
